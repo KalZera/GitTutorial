ca6cadcaafe11e48ab4abc0b078ed97cdb0e8207
KalZera <sk9.22alex@gmail.com>
2018-02-22 18:37:01 -0300

    cadastro design 90 %

--- config/express.js
+++ config/express.js
@@ -1,10 +1,11 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var path = require('path')
+var app = express();
 // var expressLayouts = require('express-ejs-layouts')
-
+app.locals.estado = require('./../model/estado.json');
 module.exports = function(){
-    var app = express();
+
     app.set('view engine', 'ejs');
     app.set('views', './views/');
 
@@ -19,17 +20,20 @@ module.exports = function(){
         })
     });
     app.get('/cadastro', function(req, res){
+        // console.log('model/estados-cidades.json');
         res.format({
             html:function(){
                 res.render('CadastroProjeto/cadastro');
             }
         })
     });
+
     app.use('/css',express.static('public/css'));
     app.use('/js',express.static('public/js'));
     app.use('/tes',express.static('node_modules/materialize-css/dist'));
     app.use('/fonts',express.static('public/fonts'));
     app.use('/jquery',express.static('node_modules/jquery/dist'));
+    app.use('/public',express.static('public'));
     
     return app;
 }
